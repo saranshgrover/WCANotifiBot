b202709a75de1cbbc80757649fbf7ae6c555c53a
saranshgrover <ycubiksrube@gmail.com>
2019-10-23 00:15:44 -0400

    added dotenv dependency

--- herokubot.py
+++ herokubot.py
@@ -18,7 +18,7 @@ def error(bot, update, error):
 
 if __name__ == "__main__":
     # Set these variable to the appropriate values
-    TOKEN = os.environ['TOKEN']
+    TOKEN = "993159315:AAFA_cNylEFenK_X1_SwMKk0RjoCNGUkFpk"
     NAME = "hidden-lake-02945"
 
     # Port is given by Heroku
